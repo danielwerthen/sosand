74de5dc1a5c0d100f1f98e107f4019ac3eacf2af
Daniel Werthén <daniel.werthen@netlight.com>
2013-12-27 17:05:06 +0100

    Test

--- scripts/grid.js
+++ scripts/grid.js
@@ -27,10 +27,14 @@
 	}
 	function placer() {
 		var grid = [];
-		return function (card, ptr) {
+		return function (card, ptr, width) {
 			var c = $(card),
-				h = _.reduce(grid[ptr.x] || [], function (m, n) { return m + n; }, 0);
-			c.css({ left: ptr.x * 200, top: h });
+				h = _.reduce(grid[ptr.x] || [], function (m, n) { return m + n; }, 0),
+				height = (c.height() / c.width()) * width;
+			c.css({ left: ptr.x * width, top: h, width: width});
+			if (c.hasClass('vimeo')) {
+				c.find('iframe').width(width).height(height);
+			}
 			if (!grid[ptr.x]) { grid[ptr.x] = []; }
 			grid[ptr.x][ptr.y] = c.outerHeight();
 		};
@@ -40,16 +44,16 @@
 		//find all cards
 		var cards = $('div.card'),
 			width = $(cards[0]).outerWidth(),
-			columns = Math.floor($(window).width() / width);
-		$('.loader').addClass('hide');
-		$('.content').addClass('show');
-		var sorted = _.sortBy(cards, function (c) { return -$(c).outerHeight(); });
+			screenw = $(window).width(),
+			columns = Math.floor(screenw / width);
+		columns = columns > 4 ? 4 : columns;
+		width = Math.floor(screenw / columns);
 		cards.addClass('absolute');
-		var it = iterator(columns > 4 ? 4 : columns),
+		var it = iterator(columns),
 			place = placer();
 		_.each(cards, function (card) {
 			var ptr = it.ptr();
-			place(card, ptr);
+			place(card, ptr, width);
 			it.step();
 		});
 	}
@@ -57,7 +61,9 @@
 	// On window load
 	window.addEventListener('load', function load() {
 		window.removeEventListener('load', load);
+		$('.content').addClass('show');
 		group();
+		$('.loader').addClass('hide');
 	});
 	window.addEventListener('resize', function load() {
 		group();
